1595ee66df4f2f15df43cbb6306a3a936696296a
Daniel Jones <messyjones@gmail.com>
2018-03-20 16:13:22 -0700

    added a test

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ app.post('/duoPasscode', function (req, res) {
     console.log(req.query.code);
 })
 
+app.get('/', (req, res) => res.send('Hello World!'))
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
